9d50903757ca140d90c3a85490236a64bd144b4f
Voidboi0163 <122555118+Voidboi0163@users.noreply.github.com>
2023-01-26 12:06:52 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+    basic.showNumber(counter, 25)
+})
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    basic.showNumber(counter,25)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+    basic.showNumber(counter, 25)
+})
+let counter = 0
+counter = 0
+basic.showNumber(counter, 25)
